3444ef860820cf0ab44549aefadab564fd955976
navies <epoon061@uottawa.ca>
2015-08-15 11:38:19 -0400

    Generalize StreamsMenu struct member names

--- src/streams.h
+++ src/streams.h
@@ -2,16 +2,12 @@
 
 #include "event.h"
 
-#define MAX_NUM_MENU_ITEMS 50
-#define MAX_STREAMER_NAME_LENGTH 25
-#define MAX_GAME_NAME_LENGTH 50
-
 typedef struct {
         MenuLayer *layer;
         uint16_t query;
         uint16_t count;
-        char **streamers;
-        char **games;
+        char **titles;
+        char **subtitles;
 } StreamsMenu;
 
 void streams_window_init(uint8_t index);
