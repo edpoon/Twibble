8fe9d78b75c395c39e07103dcdfc5878baad95cf
navies <epoon061@uottawa.ca>
2015-09-13 22:42:18 -0400

    Add icons for aplite compatability

--- src/settings.h
+++ src/settings.h
@@ -6,6 +6,4 @@ typedef struct {
     TextLayer *text;
 } AccountMenu;
 
-void account_window_init(uint8_t index);
-void down_click_config_handler(ClickRecognizerRef recognizer,void *context);
-void up_click_config_handler(ClickRecognizerRef recognizer,void *context);
+void account_window_init();
--- src/streams.c
+++ src/streams.c
@@ -3,9 +3,10 @@
 
 #define NUM_MENU_SECTIONS 1
 #define MENU_CELL_HEIGHT 55
+#define MENU_STACK_DEPTH 2 // Deepest: games -> streams
 
-static Window *streams_window;
-static StreamsMenu menu;
+static StreamsMenu menu_stack[MENU_STACK_DEPTH];
+static int8_t menu_stack_pointer = -1; // Points to the current position in the menu stack
 
 static void streams_window_load(Window *window);
 static void streams_window_unload(Window *window);
@@ -17,62 +18,65 @@ static uint16_t get_num_sections_callback(MenuLayer *menu_layer, void *ctx);
 static uint16_t get_num_rows_callback(struct MenuLayer *menu_layer, uint16_t section_index, void *callback_context);
 static int16_t get_cell_height(struct MenuLayer *menu_layer, MenuIndex *cell_index, void *data);
 
-// Viewers icon
-static GBitmap *viewer_icon;
+void streams_window_init(char *query) {
+  // New item in menu stack
+  menu_stack_pointer++;
 
-void streams_window_init(uint8_t index) {
-  // Setup menu struct
-  menu.count = 0;
-  menu.query = index;
-  menu.titles = NULL;
-  menu.subtitles1 = NULL;
-  menu.subtitles2 = NULL;
+  // Set menu properties
+  menu_stack[menu_stack_pointer].window = window_create();
+  menu_stack[menu_stack_pointer].count = 0;
+  menu_stack[menu_stack_pointer].query = query;
+  menu_stack[menu_stack_pointer].titles = NULL;
+  menu_stack[menu_stack_pointer].subtitles1 = NULL;
+  menu_stack[menu_stack_pointer].subtitles2 = NULL;
+  menu_stack[menu_stack_pointer].viewer_icon = gbitmap_create_with_resource(RESOURCE_ID_viewer);
 
-  viewer_icon = gbitmap_create_with_resource(RESOURCE_ID_viewer);
-
-  app_message_set_context(&menu);
+  //  app_message_set_context(&menu);
+  app_message_set_context(&menu_stack[menu_stack_pointer]);
 
   // Send a request to PebbleKitJS for items
-  send_message(menu.query, menu.count);
-
-  // Create streams window element and assign to pointer
-  streams_window = window_create();
+  send_message(menu_stack[menu_stack_pointer].query, menu_stack[menu_stack_pointer].count);
 
   // Set handlers to manage the elements inside the window
-  window_set_window_handlers(streams_window, (WindowHandlers) {
+  window_set_window_handlers(menu_stack[menu_stack_pointer].window, (WindowHandlers) {
       .load = streams_window_load,
         .unload = streams_window_unload,
         });
 
   // Show the window on the watch with animated = true
-  window_stack_push(streams_window, true);
+  window_stack_push(menu_stack[menu_stack_pointer].window, true);
 }
 
 static void menu_layer_select_click(struct MenuLayer *menu_layer, MenuIndex *cell_index, void *callback_context) {
-  if (menu.query == 1) {
-    streams_window_init(42);
+  if (strcmp(menu_stack[menu_stack_pointer].query, "Games") == 0) {
+    streams_window_init(menu_stack[menu_stack_pointer].titles[cell_index->row]);
   }
 }
 
 static void selection_changed(MenuLayer *menu_layer, MenuIndex new_index, MenuIndex old_index, void *callback_context) {
-  // Capping streams number of menu items at 250
-  if (menu.count % 5 == 0 && menu.count - new_index.row == 6  && old_index.row != 0 && menu.count < 250) {
-    APP_LOG(APP_LOG_LEVEL_INFO, "Row: %d, Count: %d", new_index.row, menu.count);
-    send_message(menu.query, menu.count);
+  if (menu_stack[menu_stack_pointer].count % 5 == 0 && menu_stack[menu_stack_pointer].count - new_index.row == 6  && old_index.row != 0) {
+    send_message(menu_stack[menu_stack_pointer].query, menu_stack[menu_stack_pointer].count);
   }
 }
 
 static void draw_row(GContext *ctx, const Layer *cell_layer, MenuIndex *cell_index, void *callback_context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "%d", menu_stack_pointer);
   // Draw title
-  graphics_draw_text(ctx, menu.titles[cell_index->row], fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD), GRect(5, 0, 140, 15), GTextOverflowModeTrailingEllipsis, GTextAlignmentLeft, NULL);
+  graphics_draw_text(ctx, menu_stack[menu_stack_pointer].titles[cell_index->row], fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD), GRect(5, 0, 140, 15), GTextOverflowModeTrailingEllipsis, GTextAlignmentLeft, NULL);
   // Draw first subtitle
-  graphics_draw_text(ctx, menu.subtitles1[cell_index->row], fonts_get_system_font(FONT_KEY_GOTHIC_18), GRect(5, 15, 140, 15), GTextOverflowModeTrailingEllipsis, GTextAlignmentLeft, NULL);
-  // Set transparent background
+  graphics_draw_text(ctx, menu_stack[menu_stack_pointer].subtitles1[cell_index->row], fonts_get_system_font(FONT_KEY_GOTHIC_18), GRect(5, 15, 140, 15), GTextOverflowModeTrailingEllipsis, GTextAlignmentLeft, NULL);
+
+  // Draw viewer icon according to platform
+#ifdef PBL_PLATFORM_BASALT
   graphics_context_set_compositing_mode(ctx, GCompOpSet);
-  // Draw viewer graphic
-  graphics_draw_bitmap_in_rect(ctx, viewer_icon, GRect(5, 35, 20, 20));
+  graphics_draw_bitmap_in_rect(ctx, menu_stack[menu_stack_pointer].viewer_icon, GRect(5, 35, 20, 20));
+#elif PBL_PLATFORM_APLITE
+  graphics_context_set_compositing_mode(ctx, GCompOpAssignInverted);
+  graphics_draw_bitmap_in_rect(ctx, menu_stack[menu_stack_pointer].viewer_icon, GRect(5, 37, 14, 14));
+#endif
+
   // Draw second subtitle
-  graphics_draw_text(ctx, menu.subtitles2[cell_index->row], fonts_get_system_font(FONT_KEY_GOTHIC_18), GRect(25, 32, 140, 15), GTextOverflowModeTrailingEllipsis, GTextAlignmentLeft, NULL);
+  graphics_draw_text(ctx, menu_stack[menu_stack_pointer].subtitles2[cell_index->row], fonts_get_system_font(FONT_KEY_GOTHIC_18), GRect(25, 32, 140, 15), GTextOverflowModeTrailingEllipsis, GTextAlignmentLeft, NULL);
 }
 
 static uint16_t get_num_sections_callback(MenuLayer *menu_layer, void *ctx) {
@@ -80,7 +84,7 @@ static uint16_t get_num_sections_callback(MenuLayer *menu_layer, void *ctx) {
 }
 
 static uint16_t get_num_rows_callback(MenuLayer *menu_layer, uint16_t section_index, void *ctx) {
-  return menu.count;
+  return menu_stack[menu_stack_pointer].count;
 }
 
 static int16_t get_cell_height(struct MenuLayer *menu_layer, MenuIndex *cell_index, void *ctx) {
@@ -94,10 +98,9 @@ static void streams_window_load(Window *window) {
   // Get the boundaries
   GRect bounds = layer_get_bounds(window_layer);
 
-  // streams_menu_layer = menu_layer_create(bounds);
-  menu.layer = menu_layer_create(bounds);
+  menu_stack[menu_stack_pointer].layer = menu_layer_create(bounds);
 
-  menu_layer_set_callbacks(menu.layer, NULL, (MenuLayerCallbacks) {
+  menu_layer_set_callbacks(menu_stack[menu_stack_pointer].layer, NULL, (MenuLayerCallbacks) {
       .get_num_sections = get_num_sections_callback,
         .get_num_rows = get_num_rows_callback,
         .get_cell_height = get_cell_height,
@@ -106,31 +109,32 @@ static void streams_window_load(Window *window) {
         .select_click = menu_layer_select_click
         });
 
-  menu_layer_set_click_config_onto_window(menu.layer, window);
+  menu_layer_set_click_config_onto_window(menu_stack[menu_stack_pointer].layer, window);
 
   // Add the menuLayer to the window's root layer
-  layer_add_child(window_layer, menu_layer_get_layer(menu.layer));
+  layer_add_child(window_layer, menu_layer_get_layer(menu_stack[menu_stack_pointer].layer));
 }
 
 static void streams_window_unload(Window *window) {
+  uint8_t i;
   // Clear memory
-  APP_LOG(APP_LOG_LEVEL_INFO, "%d", menu.query);
-  menu_layer_destroy(menu.layer);
-  for (int i = 0; i < menu.count; i++) {
-    free(menu.titles[i]);
-    free(menu.subtitles1[i]);
-    free(menu.subtitles2[i]);
+  menu_layer_destroy(menu_stack[menu_stack_pointer].layer);
+  for (i = 0; i < menu_stack[menu_stack_pointer].count; i++) {
+    free(menu_stack[menu_stack_pointer].titles[i]);
+    free(menu_stack[menu_stack_pointer].subtitles1[i]);
+    free(menu_stack[menu_stack_pointer].subtitles2[i]);
   }
-  free(menu.titles);
-  free(menu.subtitles1);
-  free(menu.subtitles2);
-
-  gbitmap_destroy(viewer_icon);
+  free(menu_stack[menu_stack_pointer].titles);
+  free(menu_stack[menu_stack_pointer].subtitles1);
+  free(menu_stack[menu_stack_pointer].subtitles2);
+  gbitmap_destroy(menu_stack[menu_stack_pointer].viewer_icon);
 
   // Set to NULL to avoid double free
-  menu.titles = NULL;
-  menu.subtitles1 = NULL;
-  menu.subtitles2 = NULL;
+  menu_stack[menu_stack_pointer].titles = NULL;
+  menu_stack[menu_stack_pointer].subtitles1 = NULL;
+  menu_stack[menu_stack_pointer].subtitles2 = NULL;
 
-  window_destroy(streams_window);
+  window_destroy(window);
+  // One less item in the menu stack
+  menu_stack_pointer--;
 }
