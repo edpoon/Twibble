4ff62b52199c75d784538b3865487d64171bc1b4
navies <epoon061@uottawa.ca>
2015-09-19 16:31:53 -0400

    Refactor code for handling of PebbleKitJS messages

--- src/event.c
+++ src/event.c
@@ -14,7 +14,6 @@ enum {
 void send_message(const char *query, uint8_t offset) {
   Tuplet queryTuplet = TupletCString(QUERY_KEY, query);
   Tuplet offsetTuplet = TupletInteger(OFFSET_KEY, offset);
-
   DictionaryIterator *iter;
 
   app_message_outbox_begin(&iter);
@@ -27,50 +26,37 @@ void send_message(const char *query, uint8_t offset) {
 
 // Called when a message is received from PebbleKitJS
 void in_received_handler(DictionaryIterator *received, void *context) {
-  // Complete assignment of tuples used in the app messages
-  Tuple *title_tuple = dict_find(received, TITLE_KEY);
-  Tuple *subtitle1_tuple = dict_find(received, SUBTITLE1_KEY);
-  Tuple *subtitle2_tuple = dict_find(received, SUBTITLE2_KEY);
-  Tuple *username_tuple = dict_find(received, USERNAME_KEY);
-  Tuple *logout_tuple = dict_find(received, 9001);
+  StreamsMenu *menu = (StreamsMenu *)context;
+  AccountMenu *settings = (AccountMenu *)app_message_get_context();
 
-  // If we are receiving a TITLE_KEY, we know it must have been called
-  // from the streams menu, so we know the context is a StreamsMenu,
-  // so we can perform this cast.
-  if (title_tuple) {
-    StreamsMenu *menu = (StreamsMenu *)app_message_get_context();
-    uint8_t count = menu->count;
-    menu->count++;
-
-    // Allocate memory for another pointer
-    menu->titles = realloc(menu->titles, menu->count * sizeof(char *));
-    menu->subtitles1 = realloc(menu->subtitles1, menu->count * sizeof(char *));
-    menu->subtitles2 = realloc(menu->subtitles2, menu->count * sizeof(char *));
-
-    // Allocate memory for another string
-    menu->titles[count] = malloc(strlen(title_tuple->value->cstring) + 1);
-    menu->subtitles1[count] = malloc(strlen(subtitle1_tuple->value->cstring) + 1);
-    menu->subtitles2[count] = malloc(strlen(subtitle2_tuple->value->cstring) + 1);
-
-    // Add stream to storage
-    strcpy(menu->titles[count], title_tuple->value->cstring);
-
-    // Add game to storage
-    if (subtitle1_tuple) {
-      strcpy(menu->subtitles1[count], subtitle1_tuple->value->cstring);
+  Tuple *tuple = dict_read_first(received);
+  while (tuple) {
+    switch (tuple->key) {
+    case TITLE_KEY:
+      menu->count++;
+      // Allocate memory for another pointer
+      menu->titles = realloc(menu->titles, menu->count * sizeof(char *));
+      // Allocate memory for another string
+      menu->titles[menu->count - 1] = malloc(strlen(tuple->value->cstring) + 1);
+      // Add title to storage
+      strcpy(menu->titles[menu->count - 1], tuple->value->cstring);
+      break;
+    case SUBTITLE1_KEY:
+      menu->subtitles1 = realloc(menu->subtitles1, menu->count * sizeof(char *));
+      menu->subtitles1[menu->count - 1] = malloc(strlen(tuple->value->cstring) + 1);
+      strcpy(menu->subtitles1[menu->count - 1], tuple->value->cstring);
+      break;
+    case SUBTITLE2_KEY:
+      menu->subtitles2 = realloc(menu->subtitles2, menu->count * sizeof(char *));
+      menu->subtitles2[menu->count - 1] = malloc(strlen(tuple->value->cstring) + 1);
+      strcpy(menu->subtitles2[menu->count - 1], tuple->value->cstring);
+      menu_layer_reload_data(menu->layer);
+      break;
+    case USERNAME_KEY...9001:
+      text_layer_set_text(settings->text, tuple->value->cstring);
+      break;
     }
-
-    if (subtitle2_tuple) {
-      strcpy(menu->subtitles2[count], subtitle2_tuple->value->cstring);
-    }
-
-    menu_layer_reload_data(menu->layer);
-  }
-
-  if (username_tuple || logout_tuple) {
-    AccountMenu *menu = (AccountMenu *)app_message_get_context();
-    char *text = username_tuple ? username_tuple->value->cstring : logout_tuple->value->cstring;
-    text_layer_set_text(menu->text, text);
+    tuple = dict_read_next(received);
   }
 }
 
