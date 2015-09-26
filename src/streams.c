6291c92c0babb2b3d91b7d4f030c2a3547db2541
Isaac Gollish <paris.estmagik@hotmail.fr>
2015-09-26 17:05:04 -0400

    Scrolling works for both title and subtitle
    Scrolling is calculated individually for each item.
    The items still scroll synchronously if they are both long enough

--- src/streams.c
+++ src/streams.c
@@ -8,7 +8,8 @@
 // For text-scrolling
 #define SCROLL_MENU_ITEM_WAIT_TIMER 1000
 #define SCROLL_MENU_ITEM_TIMER 400
-#define MENU_CHARS_VISIBLE 18
+#define MENU_CHARS_VISIBLE 16
+#define SUBTITLE_CHARS_VISIBLE 22
 
 static StreamsMenu menu_stack[MENU_STACK_DEPTH];
 static int8_t menu_stack_pointer = -1; // Points to the current position in the menu stack
@@ -89,18 +90,28 @@ static void draw_row(GContext *ctx, const Layer *cell_layer, MenuIndex *cell_ind
     graphics_context_set_text_color(ctx, GColorBlack);
   #endif
 
+  //Draw Title
   if (menuIndex.row == cell_index->row) {
-    graphics_draw_text(ctx, menu->titles[cell_index->row] + menu->menu_scroll_offset, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD), GRect(5, 0, 140, 15), GTextOverflowModeTrailingEllipsis, GTextAlignmentLeft, NULL);
+    graphics_draw_text(ctx,
+            (menu->scrolling_still_required) ? menu->titles[cell_index->row] + menu->menu_scroll_offset : menu->titles[cell_index->row],
+            fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD), GRect(5, 0, 140, 15), GTextOverflowModeTrailingEllipsis, GTextAlignmentLeft, NULL);
   }
   else {
-    graphics_draw_text(ctx, menu->titles[cell_index->row], fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD), GRect(5, 0, 140, 15), GTextOverflowModeTrailingEllipsis, GTextAlignmentLeft, NULL);
+    graphics_draw_text(ctx, menu->titles[cell_index->row],
+            fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD), GRect(5, 0, 140, 15), GTextOverflowModeTrailingEllipsis, GTextAlignmentLeft, NULL);
   }
+
+
   // Draw first subtitle
    if (menuIndex.row == cell_index->row) {
-     graphics_draw_text(ctx, menu->subtitles1[cell_index->row] + menu->menu_scroll_offset, fonts_get_system_font(FONT_KEY_GOTHIC_18), GRect(5, 15, 140, 15), GTextOverflowModeTrailingEllipsis, GTextAlignmentLeft, NULL);
-   } else {
-     graphics_draw_text(ctx, menu->subtitles1[cell_index->row], fonts_get_system_font(FONT_KEY_GOTHIC_18), GRect(5, 15, 140, 15), GTextOverflowModeTrailingEllipsis, GTextAlignmentLeft, NULL);
+     graphics_draw_text(ctx,
+             (menu->subtitle_scrolling_required) ? menu->subtitles1[cell_index->row] + menu->menu_scroll_offset : menu->subtitles1[cell_index->row],
+             fonts_get_system_font(FONT_KEY_GOTHIC_18), GRect(5, 15, 140, 15), GTextOverflowModeTrailingEllipsis, GTextAlignmentLeft, NULL);
    }
+  else {
+    graphics_draw_text(ctx, menu->subtitles1[cell_index->row],
+            fonts_get_system_font(FONT_KEY_GOTHIC_18), GRect(5, 15, 140, 15), GTextOverflowModeTrailingEllipsis, GTextAlignmentLeft, NULL);
+  }
 
   // Draw viewer icon according to platform
 #ifdef PBL_PLATFORM_BASALT
@@ -114,10 +125,16 @@ static void draw_row(GContext *ctx, const Layer *cell_layer, MenuIndex *cell_ind
   // Draw second subtitle
   graphics_draw_text(ctx, menu->subtitles2[cell_index->row], fonts_get_system_font(FONT_KEY_GOTHIC_18), GRect(25, 32, 140, 15), GTextOverflowModeTrailingEllipsis, GTextAlignmentLeft, NULL);
 
-  int subtitle1_length = strlen(menu->titles[menuIndex.row]);
+  //Need to determine whether to keep scrolling
+  int title_length = strlen(menu->titles[menuIndex.row]);
+  int subtitle1_length = strlen(menu->subtitles1[menuIndex.row]);
 
- if (subtitle1_length - MENU_CHARS_VISIBLE - menu->menu_scroll_offset > 0) {
+  if (title_length - MENU_CHARS_VISIBLE - menu->menu_scroll_offset > 0) {
     menu->scrolling_still_required = true;
+  }
+
+  if (subtitle1_length - SUBTITLE_CHARS_VISIBLE - menu->menu_scroll_offset > 0) {
+    menu->subtitle_scrolling_required = true;
   }
 }
 
@@ -188,6 +205,7 @@ static void initiate_menu_scroll_timer(StreamsMenu* menu_ptr) {
   // If there is already a timer then reschedule it, otherwise create one
   bool need_to_create_timer = true;
   menu_ptr->scrolling_still_required = false;
+  menu_ptr->subtitle_scrolling_required = false;
   menu_ptr->menu_scroll_offset = 0;
   menu_ptr->menu_reloading_to_scroll = false;
   if (menu_ptr->menu_scroll_timer) {
@@ -213,13 +231,14 @@ static void scroll_menu_callback(void* data) {
 
   menu->menu_scroll_timer = NULL;
   menu->menu_scroll_offset++;
-  if (!menu->scrolling_still_required) {
+  if (!menu->scrolling_still_required && !menu->subtitle_scrolling_required) {
     return;
   }
   if(menuIndex.row != 0) {
     menu->menu_reloading_to_scroll = true;
   }
   menu->scrolling_still_required = false;
+  menu->subtitle_scrolling_required = false;
   menu_layer_reload_data(menu->layer);
   menu->menu_scroll_timer = app_timer_register(SCROLL_MENU_ITEM_TIMER, scroll_menu_callback, menu);
 }
--- src/streams.h
+++ src/streams.h
@@ -15,6 +15,7 @@ typedef struct {
 
   // The following are for text-scrolling
   bool scrolling_still_required;
+  bool subtitle_scrolling_required;
   int menu_scroll_offset;
   bool menu_reloading_to_scroll;
   AppTimer* menu_scroll_timer;
