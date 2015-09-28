6136e7a075ac8dba32a8dcf9b0546a1ebcb42be0
navies <epoon061@uottawa.ca>
2015-09-27 20:40:31 -0400

    Create seperate files for main menu

---
+++ src/home.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "streams.h"
+
+#define NUM_MENU_SECTIONS 1
+#define NUM_MENU_ITEMS 4
+
+typedef struct {
+  Window *window;
+  MenuLayer *layer;
+  char *titles[NUM_MENU_ITEMS];
+  GBitmap *icons[NUM_MENU_ITEMS];
+} HomeMenu;
+
+void init(void);
