fc235c1ab3cb47e3471a90497a9593b88c368c91
navies <epoon061@uottawa.ca>
2015-10-10 22:00:39 -0400

    Destroy error screen components when window unloads

--- src/event.c
+++ src/event.c
@@ -24,34 +24,69 @@ void send_message(const char *query, uint8_t offset) {
   app_message_outbox_send();
 }
 
-// Error message generated upon failure to retrieve proper info from API
-void display_error(char *error_message) {
-  APP_LOG(APP_LOG_LEVEL_INFO, "display error");
-  Window *window = window_stack_get_top_window();
+typedef struct {
+  GBitmap *image;
+  BitmapLayer *image_layer;
+  TextLayer *message_layer;
+} ErrorScreen;
+
+void error_window_load(Window *window) {
+  char *error_message = window_get_user_data(window);
+  ErrorScreen *error = malloc(sizeof(ErrorScreen));
+  memset(error, 0, sizeof(ErrorScreen));
+
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  GBitmap *error_image = gbitmap_create_with_resource(RESOURCE_ID_error);
-  GRect error_image_bounds = gbitmap_get_bounds(error_image);
-  BitmapLayer *error_layer = bitmap_layer_create(GRect(0, 0, bounds.size.w, error_image_bounds.size.h));
+  // Draw error image
+  error->image = gbitmap_create_with_resource(RESOURCE_ID_error);
+  GRect error_image_bounds = gbitmap_get_bounds(error->image);
+  error->image_layer = bitmap_layer_create(GRect(0, 0, bounds.size.w, error_image_bounds.size.h));
 #ifdef PBL_PLATFORM_BASALT
-  bitmap_layer_set_compositing_mode(error_layer, GCompOpSet);
+  bitmap_layer_set_compositing_mode(error->image_layer, GCompOpSet);
 #elif PBL_PLATFORM_APLITE
-  bitmap_layer_set_compositing_mode(error_layer, GCompOpAssignInverted);
+  bitmap_layer_set_compositing_mode(error->image_layer, GCompOpAssignInverted);
 #endif
-  bitmap_layer_set_bitmap(error_layer, error_image);
-  layer_add_child(window_layer, bitmap_layer_get_layer(error_layer));
+  bitmap_layer_set_bitmap(error->image_layer, error->image);
+  layer_add_child(window_layer, bitmap_layer_get_layer(error->image_layer));
 
-  TextLayer *error_message_layer = text_layer_create(GRect(0, error_image_bounds.size.h, bounds.size.w, bounds.size.h - error_image_bounds.size.h));
-  text_layer_set_text(error_message_layer, error_message);
-  text_layer_set_text_alignment(error_message_layer, GTextAlignmentCenter);
-  text_layer_set_font(error_message_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
-  layer_add_child(window_layer, text_layer_get_layer(error_message_layer));
+  // Draw error message
+  error->message_layer = text_layer_create(GRect(0, error_image_bounds.size.h, bounds.size.w, bounds.size.h - error_image_bounds.size.h));
+  text_layer_set_text(error->message_layer, error_message);
+  text_layer_set_text_alignment(error->message_layer, GTextAlignmentCenter);
+  text_layer_set_font(error->message_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
 
+  layer_add_child(window_layer, text_layer_get_layer(error->message_layer));
+  window_set_user_data(window, error);
+}
+
+void error_window_unload(Window *window) {
+  ErrorScreen *error = window_get_user_data(window);
+  gbitmap_destroy(error->image);
+  bitmap_layer_destroy(error->image_layer);
+  text_layer_destroy(error->message_layer);
+  free(error);
+  window_destroy(window);
+}
+
+// Error message generated upon failure to retrieve proper info from API
+static void display_error(char *error_message) {
+  Window *window = window_create();
+  window_set_window_handlers(window, (WindowHandlers) {
+      .load = error_window_load,
+        .unload = error_window_unload,
+    });
+  window_set_user_data(window, error_message);
+
+  bool animated = false;
+  window_stack_pop(animated);
+  window_stack_push(window, animated);
 }
 
 // Called when a message is received from PebbleKitJS
 void in_received_handler(DictionaryIterator *received, void *context) {
+  StreamsMenu *menu = (StreamsMenu *)context;
+
   // Check if there was an error in retrieving information
   Tuple *error_tuple = dict_find(received, ERROR_KEY);
   if (error_tuple) {
@@ -59,7 +94,6 @@ void in_received_handler(DictionaryIterator *received, void *context) {
     return;
   }
 
-  StreamsMenu *menu = (StreamsMenu *)context;
   Tuple *tuple = dict_read_first(received);
   menu->count++;
   while (tuple) {
